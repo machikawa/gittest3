4bac10eaee29b955c7bba19a0629d10cb61d4457
Townriver <a1938hm@aiit.ac.jp>
2022-02-15 08:38:16 +0900

    Initial Commit

---
+++ gittest3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gittest3
+//
+//  Created by たうんりばー on 2022/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
